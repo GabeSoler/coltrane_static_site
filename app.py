0312727caff5bf3accd3f58a075d36a2b141f5e5
Gabriel Soler <gsoler@gmail.com>
2024-07-18 18:33:47 +0100

    changed allowed hosts

--- app.py
+++ app.py
@@ -5,12 +5,18 @@
 from django.core.management import execute_from_command_line
 
 from coltrane import initialize
+import os
 
 
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
 
-wsgi = initialize(ALLOWED_HOSTS = ['0.0.0.0',
-                                   
-                                   ], 
+
+wsgi = initialize(
+    ALLOWED_HOSTS=ALLOWED_HOSTS
                                    )
 
 if __name__ == "__main__":
