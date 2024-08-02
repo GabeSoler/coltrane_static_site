7af0ff13e19ad32ec278d4ec444a1b8866e9cad8
Gabriel Soler <gsoler@gmail.com>
2024-08-02 18:38:33 +0100

    moved a function up in gunicorn.conf.py to see if speed increases

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,9 @@
 import multiprocessing
 
+def when_ready(server):
+    # touch app-initialized when ready
+    open("/tmp/app-initialized", "w").close()
+
 max_requests = 1000
 max_requests_jitter = 50
 
@@ -9,11 +13,6 @@ bind = "0.0.0.0:80"
 workers = multiprocessing.cpu_count() * 2 + 1
 
 workers = 2
-
-
-def when_ready(server):
-    # touch app-initialized when ready
-    open("/tmp/app-initialized", "w").close()
 
 
 bind = "unix:///tmp/nginx.socket"
